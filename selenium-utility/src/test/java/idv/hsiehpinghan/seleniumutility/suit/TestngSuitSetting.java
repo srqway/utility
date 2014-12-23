decb76b1fa69e71e7dd0d4450a8e7bca49cae75e
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2014-12-23 16:03:05 +0800

    Create selenium-utility project.

---
+++ selenium-utility/src/test/java/idv/hsiehpinghan/seleniumutility/suit/TestngSuitSetting.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.seleniumutility.suit;
+
+import org.testng.annotations.BeforeSuite;
+
+public class TestngSuitSetting {
+	@BeforeSuite()
+	public void beforeSuite() {
+	}
+
+}
---
+++ selenium-utility/src/test/java/idv/hsiehpinghan/seleniumutility/utility/ResourceUtilityTest.java
@@ -0,0 +1,16 @@
+package idv.hsiehpinghan.seleniumutility.utility;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ResourceUtilityTest {
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+}
