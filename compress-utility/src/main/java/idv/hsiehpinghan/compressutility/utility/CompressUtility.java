204f43d157dca474eff104ebe2cb12bfca318b80
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2014-12-27 16:31:43 +0800

    Backup.

--- compress-utility/src/main/java/idv/hsiehpinghan/compressutility/utility/CompressUtility.java
+++ compress-utility/src/main/java/idv/hsiehpinghan/compressutility/utility/CompressUtility.java
@@ -7,15 +7,22 @@ import net.lingala.zip4j.exception.ZipException;
 
 public class CompressUtility {
 	/**
-	 * Unzip file to extractDirectory/file.getName().
+	 * Unzip file to extractDirectory/file.getName() and return it.
+	 * 
 	 * @param file
 	 * @param extractDirectory
+	 * @return
 	 * @throws ZipException
 	 */
-	public static void unzip(File file, File extractDirectory)
+	public static File unzip(File file, File extractDirectory)
 			throws ZipException {
 		ZipFile zip = new ZipFile(file);
 		String dir = extractDirectory.getAbsolutePath() + "/" + file.getName();
-		zip.extractAll(dir);
+		File target = new File(dir);
+		if (target.exists() == false) {
+			target.mkdir();
+		}
+		zip.extractAll(target.getAbsolutePath());
+		return target;
 	}
 }
