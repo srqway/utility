458aa625550bfb651390e0f6374ba5858990a21f
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2014-12-31 17:21:59 +0800

    Backup.

--- data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/BooleanUtility.java
+++ data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/BooleanUtility.java
@@ -2,4 +2,5 @@ package idv.hsiehpinghan.datatypeutility.utility;
 
 public class BooleanUtility {
 	public static final boolean BOOLEAN_DEFAULT_VALUE = false;
+	public static final int BOOLEAN_BYTE_AMOUNT = 1;
 }
--- data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/ByteUtility.java
+++ data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/ByteUtility.java
@@ -2,4 +2,5 @@ package idv.hsiehpinghan.datatypeutility.utility;
 
 public class ByteUtility {
 	public static final byte BYTE_DEFAULT_VALUE = 0;
+	public static final int BYTE_BYTE_AMOUNT = 1;
 }
