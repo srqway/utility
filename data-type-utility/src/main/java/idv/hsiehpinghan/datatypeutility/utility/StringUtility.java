97fdff5556f24a2c432e47e3ec2cbcafcc146f32
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-01-19 11:32:06 +0800

    Backup.

--- data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/StringUtility.java
+++ data-type-utility/src/main/java/idv/hsiehpinghan/datatypeutility/utility/StringUtility.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.IOUtils;
 
 public class StringUtility {
 	public static final String EMPTY_STRING = "";
+	public static final String COMMA_STRING = ",";
 	
 	/**
 	 * Convert inputstream to string.
