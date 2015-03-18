ccbd1cfdc92ff1f895111509eda0f33e001d71bf
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-03-18 13:59:10 +0800

    Backup.

--- resource-utility/src/main/java/idv/hsiehpinghan/resourceutility/utility/CsvUtility.java
+++ resource-utility/src/main/java/idv/hsiehpinghan/resourceutility/utility/CsvUtility.java
@@ -1,5 +1,7 @@
 package idv.hsiehpinghan.resourceutility.utility;
 
+import idv.hsiehpinghan.datatypeutility.utility.CharsetUtility;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -10,6 +12,13 @@ import org.apache.commons.csv.CSVRecord;
 
 public class CsvUtility {
 	// private Logger logger = Logger.getLogger(this.getClass().getName());
+	private static final Charset UTF8 = CharsetUtility.UTF_8;
+	private static final CSVFormat EXCEL = CSVFormat.EXCEL;
+
+	public static CSVParser getParserAtDataStartRow(File file,
+			String[] targetTitles) throws IOException {
+		return getParserAtDataStartRow(file, UTF8, EXCEL, targetTitles);
+	}
 
 	public static CSVParser getParserAtDataStartRow(File file, Charset charset,
 			CSVFormat format, String[] targetTitles) throws IOException {
