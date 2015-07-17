48c57599d249eb09b4d959e255ff0d31c311b9b0
thank <thank@localhost>
2015-07-16 23:35:10 -0700

    backup.

---
+++ collection-utility/src/test/java/idv/hsiehpinghan/collectionutility/valueobject/HashCodeAndEquals.java
@@ -0,0 +1,45 @@
+package idv.hsiehpinghan.collectionutility.valueobject;
+
+public class HashCodeAndEquals {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HashCodeAndEquals other = (HashCodeAndEquals) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
