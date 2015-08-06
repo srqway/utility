12658bd4d04b201297916ddd06afd77f9064e0d4
thank <thank@localhost>
2015-08-06 00:36:13 -0700

    backup.

--- collection-utility/src/test/java/idv/hsiehpinghan/collectionutility/java/util/HashSetTest.java
+++ collection-utility/src/test/java/idv/hsiehpinghan/collectionutility/java/util/HashSetTest.java
@@ -15,18 +15,21 @@ public class HashSetTest {
 		final int SIZE = 2;
 		Set<HashCodeAndEquals> set = new HashSet<HashCodeAndEquals>();
 		for (int i = 0, size = SIZE; i < size; ++i) {
-			set.add(generateHashCodeAndEquals(i));
+			set.add(generateHashCodeAndEquals(i, "first"));
 		}
 		for (int i = 0, size = SIZE; i < size; ++i) {
-			set.add(generateHashCodeAndEquals(i));
+			set.add(generateHashCodeAndEquals(i, "second"));
 		}
 		Assert.assertEquals(SIZE, set.size());
+		for (HashCodeAndEquals item : set) {
+			Assert.assertTrue(item.getName().startsWith("first"));
+		}
 	}
 
-	private HashCodeAndEquals generateHashCodeAndEquals(int i) {
+	private HashCodeAndEquals generateHashCodeAndEquals(int i, String name) {
 		HashCodeAndEquals obj = new HashCodeAndEquals();
 		obj.setId(i);
-		obj.setName("name_" + i);
+		obj.setName(name + "_" + i);
 		return obj;
 	}
 }
