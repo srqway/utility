d148c82064964e7437db71624c71cf3798ca017f
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-12-22 23:47:10 +0800

    backup.

--- datetime-utility/src/main/java/idv/hsiehpinghan/datetimeutility/utility/LocalDateTimeUtility.java
+++ datetime-utility/src/main/java/idv/hsiehpinghan/datetimeutility/utility/LocalDateTimeUtility.java
@@ -2,6 +2,7 @@ package idv.hsiehpinghan.datetimeutility.utility;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class LocalDateTimeUtility {
@@ -13,6 +14,11 @@ public class LocalDateTimeUtility {
 		Date date = new Date(time);
 		return LocalDateTime
 				.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	}
+
+	public static String toString(LocalDateTime localDateTime, String pattern) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+		return localDateTime.format(formatter);
 	}
 
 }
