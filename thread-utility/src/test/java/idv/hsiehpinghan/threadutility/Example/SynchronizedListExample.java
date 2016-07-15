7499a8f2ad8c69413277b8fd36e13c1157be42c6
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-07-15 12:23:09 +0800

    backup.

--- thread-utility/src/test/java/idv/hsiehpinghan/threadutility/Example/SynchronizedListExample.java
+++ thread-utility/src/test/java/idv/hsiehpinghan/threadutility/Example/SynchronizedListExample.java
@@ -1,14 +1,16 @@
 package idv.hsiehpinghan.threadutility.Example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public class SynchronizedBlockExample implements Runnable {
+public class SynchronizedListExample implements Runnable {
 	private static final long FIVE_SECONDS = 5000;
 	private String name;
 	private SynchronizedBlockClass obj;
 
-	public SynchronizedBlockExample(String name, SynchronizedBlockClass obj) {
+	public SynchronizedListExample(String name, SynchronizedBlockClass obj) {
 		this.name = name;
 		this.obj = obj;
 	}
@@ -19,22 +21,31 @@ public class SynchronizedBlockExample implements Runnable {
 			System.err.printf("%s add %d%n", name, i);
 			obj.add(i);
 		}
+		obj.printIntegerList();
 	}
 
 	public static class SynchronizedBlockClass {
-		private List<Integer> integerList = new ArrayList<>();
+		List<Integer> integerList = Collections.synchronizedList(new ArrayList<Integer>());
 
 		public void add(Integer integer) {
+			integerList.add(integer);
+		}
+
+		public void printIntegerList() {
 			synchronized (integerList) {
-				integerList.add(integer);
+				Iterator<Integer> iter = integerList.iterator();
+				while (iter.hasNext()) {
+					System.err.printf("%d%n", iter.next());
+				}
+				System.err.println();
 			}
 		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		SynchronizedBlockClass obj = new SynchronizedBlockClass();
-		Thread thread_0 = new Thread(new SynchronizedBlockExample("thread_0", obj));
-		Thread thread_1 = new Thread(new SynchronizedBlockExample("thread_1", obj));
+		Thread thread_0 = new Thread(new SynchronizedListExample("thread_0", obj));
+		Thread thread_1 = new Thread(new SynchronizedListExample("thread_1", obj));
 		thread_0.setDaemon(true);
 		thread_1.setDaemon(true);
 		thread_0.start();
