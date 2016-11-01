9fbc3a130eac6a66e0adb83f7b1e7bc2b7f1f428
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-01 14:13:38 +0800

    backup.

--- thread-utility/src/test/java/idv/hsiehpinghan/threadutility/example/ExecutorServiceExample.java
+++ thread-utility/src/test/java/idv/hsiehpinghan/threadutility/example/ExecutorServiceExample.java
@@ -3,6 +3,51 @@ package idv.hsiehpinghan.threadutility.example;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 資料來源：JAVA 我的程式比你的快10倍 從概念到工具的極度優化 
+ * 
+ * Executors.newFixedThreadPool()：
+ * 該方法傳回一個固定執行緒數量的執行緒池，
+ * 該執行緒池中的執行緒數量不變。
+ * 當有一個新的工作傳送時，
+ * 執行緒池中若有空閒執行緒，
+ * 則立即執行。
+ * 若沒有，
+ * 則新的工作會被暫存在一個工作佇列中，
+ * 待有執行緒空閒時，
+ * 便處理在工作佇列中的工作。
+ * 
+ * Executors.newSingleThreadExecutor()方法：
+ * 該方法傳回一個只有一個執行緒的執行緒池。
+ * 若多餘一個工作被傳送到該執行緒池，
+ * 工作會被儲存在一個工作佇列中，
+ * 待執行緒空閒，
+ * 按先入先出的循序執行佇列中的工作。
+ * 
+ * Executors.newCachedThreadPool()方法：
+ * 該方法傳回一個可根據實際情況調整執行緒數量的執行緒池。
+ * 執行緒池的執行緒數量不確定，
+ * 但若有空閒執行緒可以多工，
+ * 則會優先使用可多工的執行緒。
+ * 若所有執行緒均在工作，
+ * 又有新的工作傳送，
+ * 則會建立新的執行緒工作。
+ * 所有執行緒在目前工作執行完畢後，
+ * 將傳回執行緒池進行多工。
+ * 
+ * Executors.newSingleThreadScheduledExecutor()方法：
+ * 該方法傳回一個ScheduledExecutorService物件，
+ * 執行緒池大小為1。
+ * ScheduledExecutorService介面在ExecutorService介面上擴充了在指定時間執行某工作的功能，
+ * 如在某個固定的延遲時間之後執行，
+ * 或週期性執行某個工作。
+ * 
+ * Executors.newScheduledThreadPool()方法：
+ * 該方法也傳回一個ScheduledExecutorService物件，
+ * 但該執行緒池可以指定執行緒數量。
+ * @author thank
+ *
+ */
 public class ExecutorServiceExample implements Runnable {
 	private static final int SIZE = 3;
 	private static final long ONE_SECOND = 1000;
