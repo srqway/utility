fe005cd7b0dbafcf9a28e33421795a8e83e0bf1f
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-12-27 17:27:26 +0800

    backup.

--- datetime-utility/src/test/java/idv/hsiehpinghan/datetimeutility/utility/DateUtilityTest.java
+++ datetime-utility/src/test/java/idv/hsiehpinghan/datetimeutility/utility/DateUtilityTest.java
@@ -9,13 +9,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DateUtilityTest {
-	private static Pattern PATTERN_DD_MMMM_YYYY = Pattern.compile("(\\d{2} [a-zA-Z]{4,} \\d{4})");
-	private static SimpleDateFormat SIMPLE_DATE_FORMAT__DD_MMMM_YYYY = new SimpleDateFormat("dd MMMM yyyy");
+	private static Pattern PATTERN_0 = Pattern.compile("(\\d{2} [a-zA-Z]{4,} \\d{4})");
+	private static SimpleDateFormat SIMPLE_DATE_FORMAT_0 = new SimpleDateFormat("dd MMMM yyyy");
+	private static Pattern PATTERN_1 = Pattern.compile("(\\d{4}-\\d{1,2}-\\d{1,2}T\\d{1,2}:\\d{1,2}:\\d{1,2})\\+0000");
+	private static SimpleDateFormat SIMPLE_DATE_FORMAT_1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
 	@Test
 	public void getDate() throws ParseException {
 		Date date = null;
-		date = DateUtility.getDate(PATTERN_DD_MMMM_YYYY, SIMPLE_DATE_FORMAT__DD_MMMM_YYYY, "23 October 2016");
+		date = DateUtility.getDate(PATTERN_0, SIMPLE_DATE_FORMAT_0, "23 October 2016");
 		Assert.assertEquals(date.toString(), "Sun Oct 23 00:00:00 CST 2016");
+		date = DateUtility.getDate(PATTERN_1, SIMPLE_DATE_FORMAT_1, "2016-2-27T01:00:36+0000");
+		Assert.assertEquals(date.toString(), "Sat Feb 27 01:00:36 CST 2016");
+
 	}
+
 }
