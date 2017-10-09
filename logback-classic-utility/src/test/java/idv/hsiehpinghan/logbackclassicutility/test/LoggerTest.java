ce131f0f371696114149e09e89f2f020f748546f
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-10-09 12:09:44 +0800

    backup.

---
+++ logback-classic-utility/src/test/java/idv/hsiehpinghan/logbackclassicutility/test/LoggerTest.java
@@ -0,0 +1,44 @@
+package idv.hsiehpinghan.logbackclassicutility.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+import org.testng.annotations.Test;
+
+public class LoggerTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
+	private static final Marker MARKER = MarkerFactory.getMarker("SEND_MAIL_MARKER");
+	private static final long ONE_SECOND = 1000;
+	private static final int LOOP_AMOUNT = 1;
+
+	@Test
+	public void basicTest() throws Exception {
+		for (int i = 0; i < LOOP_AMOUNT; ++i) {
+			LOGGER.trace("trace");
+			LOGGER.debug("debug");
+			LOGGER.info("info");
+			LOGGER.warn("warn");
+			LOGGER.error("error");
+			Thread.sleep(ONE_SECOND);
+		}
+	}
+
+	@Test
+	public void markerTest() throws Exception {
+		for (int i = 0; i < LOOP_AMOUNT; ++i) {
+			LOGGER.trace("NO MARKER : trace");
+			LOGGER.trace(MARKER, "MARKER : trace");
+			LOGGER.debug("NO MARKER : debug");
+			LOGGER.debug(MARKER, "MARKER : debug");
+			LOGGER.info("NO MARKER : info");
+			LOGGER.info(MARKER, "MARKER : info");
+			LOGGER.warn("NO MARKER : warn");
+			LOGGER.warn(MARKER, "MARKER : warn");
+			LOGGER.error("NO MARKER : error");
+			LOGGER.error(MARKER, "MARKER : error");
+			Thread.sleep(ONE_SECOND);
+		}
+
+	}
+}
