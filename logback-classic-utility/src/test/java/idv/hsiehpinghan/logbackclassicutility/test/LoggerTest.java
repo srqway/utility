84461a652d14d50a9f286d437727eb9c3325a7de
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-10-09 17:06:27 +0800

    backup.

--- logback-classic-utility/src/test/java/idv/hsiehpinghan/logbackclassicutility/test/LoggerTest.java
+++ logback-classic-utility/src/test/java/idv/hsiehpinghan/logbackclassicutility/test/LoggerTest.java
@@ -2,6 +2,7 @@ package idv.hsiehpinghan.logbackclassicutility.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ public class LoggerTest {
 	private static final Marker MARKER = MarkerFactory.getMarker("SEND_MAIL_MARKER");
 	private static final long ONE_SECOND = 1000;
 	private static final int LOOP_AMOUNT = 1;
+	private static final String MDC_KEY = "MDC_KEY";
 
 	@Test
 	public void basicTest() throws Exception {
@@ -39,6 +41,24 @@ public class LoggerTest {
 			LOGGER.error(MARKER, "MARKER : error");
 			Thread.sleep(ONE_SECOND);
 		}
-
+	}
+	
+	@Test
+	public void mdcTest() throws Exception {
+		final Logger LOGGER = LoggerFactory.getLogger(LoggerTest.class);
+		for (int i = 0; i < LOOP_AMOUNT; ++i) {
+			LOGGER.trace("trace");
+			LOGGER.debug("debug");
+			LOGGER.info("info");
+			LOGGER.warn("warn");
+			LOGGER.error("error");
+			MDC.put(MDC_KEY, "MDC_VALUE");
+			LOGGER.trace("MDC : trace");
+			LOGGER.debug("MDC : debug");
+			LOGGER.info("MDC : info");
+			LOGGER.warn("MDC : warn");
+			LOGGER.error("MDC : error");
+			Thread.sleep(ONE_SECOND);
+		}
 	}
 }
